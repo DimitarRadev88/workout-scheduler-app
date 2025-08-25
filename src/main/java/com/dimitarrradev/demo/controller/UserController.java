942a18b55d5215e6a903063cc16ff786daa68062
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2025-08-25 16:41:22 +0300

    Add: validation for registration

--- src/main/java/com/dimitarrradev/demo/controller/UserController.java
+++ src/main/java/com/dimitarrradev/demo/controller/UserController.java
@@ -3,11 +3,16 @@ package com.dimitarrradev.demo.controller;
 import com.dimitarrradev.demo.controller.model.UserLoginBindingModel;
 import com.dimitarrradev.demo.controller.model.UserRegisterBindingModel;
 import com.dimitarrradev.demo.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -27,18 +32,26 @@ public class UserController {
     }
 
     @GetMapping("/register")
-    public ModelAndView getHomePage(ModelAndView modelAndView) {
-        modelAndView.getModel().put("userRegister", new UserRegisterBindingModel(null, null, null, null));
-        modelAndView.setViewName("register");
+    public String getHomePage(Model model) {
+        if (!model.containsAttribute("userRegister")) {
+            model.addAttribute("userRegister", new UserRegisterBindingModel("", "", "", ""));
+        }
 
-        return modelAndView;
+        return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(UserRegisterBindingModel userRegisterBindingModel) {
-        userService.doRegister(userRegisterBindingModel);
+    public ModelAndView registerUser(ModelAndView modelAndView, @Valid @ModelAttribute("userRegister") UserRegisterBindingModel userRegister, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userRegister", userRegister);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegister", bindingResult);
+            modelAndView.setViewName("redirect:/users/register");
+        } else {
+            userService.doRegister(userRegister);
+            modelAndView.setViewName("redirect:/users/login");
+        }
 
-        return "redirect:/users/login";
+        return modelAndView;
     }
 
 }
--- src/main/java/com/dimitarrradev/demo/controller/model/UserRegisterBindingModel.java
+++ src/main/java/com/dimitarrradev/demo/controller/model/UserRegisterBindingModel.java
@@ -1,9 +1,12 @@
 package com.dimitarrradev.demo.controller.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public record UserRegisterBindingModel(
-        String username,
-        String email,
-        String password,
-        String confirmPassword
+        @Size(min = 2, max = 30, message = "Username length must be between 2 and 30 symbols") String username,
+        @Email String email,
+        @Size(min = 6, max = 30, message = "Password length must be between 6 and 30 symbols") String password,
+        @Size(min = 6, max = 30, message = "Password length must be between 6 and 30 symbols") String confirmPassword
 ) {
 }

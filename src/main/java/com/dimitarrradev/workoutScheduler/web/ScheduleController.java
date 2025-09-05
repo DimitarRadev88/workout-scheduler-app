92ce851c4dc2acf9eab134d1303e013444db8f0b
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2025-09-05 11:01:56 +0300

    Add: schedules page and MVC endpoint for it

---
+++ src/main/java/com/dimitarrradev/workoutScheduler/web/ScheduleController.java
@@ -0,0 +1,23 @@
+package com.dimitarrradev.workoutScheduler.web;
+
+import com.dimitarrradev.workoutScheduler.schedule.service.ScheduleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/schedules")
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @GetMapping()
+    public String getSchedulePage() {
+        return "schedules";
+    }
+
+}
